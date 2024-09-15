b1c0fd33ec6941e1826eb3f30beff0af115fe415
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-09-16 01:36:58 +0530

    Update merge_right.rs

--- src/core/merge_right.rs
+++ src/core/merge_right.rs
@@ -1,14 +1,15 @@
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
 use std::sync::Arc;
 
+use crate::core::valid::{Valid, Validator};
 use serde_yaml::Value;
 
 pub trait MergeRight {
-    fn merge_right(self, other: Self) -> Self;
+    fn merge_right(self, other: Self) -> Valid<Self, String>;
 }
 
 impl<A: MergeRight> MergeRight for Option<A> {
-    fn merge_right(self, other: Self) -> Self {
+    fn merge_right(self, other: Self) -> Valid<Self, String> {
         match (self, other) {
             (Some(this), Some(that)) => Some(this.merge_right(that)),
             (None, Some(that)) => Some(that),
@@ -19,7 +20,7 @@ impl<A: MergeRight> MergeRight for Option<A> {
 }
 
 impl<A: MergeRight + Default> MergeRight for Arc<A> {
-    fn merge_right(self, other: Self) -> Self {
+    fn merge_right(self, other: Self) -> Valid<Self, String> {
         let l = Arc::into_inner(self);
         let r = Arc::into_inner(other);
         Arc::new(l.merge_right(r).unwrap_or_default())
@@ -27,9 +28,9 @@ impl<A: MergeRight + Default> MergeRight for Arc<A> {
 }
 
 impl<A> MergeRight for Vec<A> {
-    fn merge_right(mut self, other: Self) -> Self {
+    fn merge_right(mut self, other: Self) -> Valid<Self, String> {
         self.extend(other);
-        self
+        Valid::succeed(self)
     }
 }
 
@@ -38,15 +39,29 @@ where
     K: Ord,
     V: Clone + MergeRight,
 {
-    fn merge_right(mut self, other: Self) -> Self {
-        for (other_name, mut other_value) in other {
-            if let Some(self_value) = self.remove(&other_name) {
-                other_value = self_value.merge_right(other_value);
-            }
+    fn merge_right(mut self, other: Self) -> Valid<Self, String> {
+        let mut errors = ValidationError::empty();
 
-            self.insert(other_name, other_value);
+        for (other_key, other_value) in other {
+            if let Some(self_value) = self.remove(&other_key) {
+                match self_value.merge_right(other_value).to_result() {
+                    Ok(merged_value) => {
+                        self.insert(other_key, merged_value);
+                    }
+                    Err(err) => {
+                        errors = errors.combine(err);
+                    }
+                }
+            } else {
+                self.insert(other_key, other_value);
+            }
         }
-        self
+
+        if errors.is_empty() {
+            Valid::succeed(self)
+        } else {
+            Valid::from_validation_err(errors)
+        }
     }
 }
 
@@ -54,9 +69,9 @@ impl<V> MergeRight for BTreeSet<V>
 where
     V: Ord,
 {
-    fn merge_right(mut self, other: Self) -> Self {
+    fn merge_right(mut self, other: Self) -> Valid<Self, String> {
         self.extend(other);
-        self
+        Valid::succeed(self)
     }
 }
 
@@ -70,56 +85,62 @@ where
     }
 }
 
-impl<K, V> MergeRight for HashMap<K, V>
-where
-    K: Eq + std::hash::Hash,
-{
-    fn merge_right(mut self, other: Self) -> Self {
-        self.extend(other);
-        self
-    }
-}
-
 impl MergeRight for Value {
-    fn merge_right(self, other: Self) -> Self {
+    fn merge_right(self, other: Self) -> Valid<Self, String> {
         match (self, other) {
-            (Value::Null | Value::Bool(_) | Value::Number(_) | Value::String(_), other) => other,
+            (Value::Null | Value::Bool(_) | Value::Number(_) | Value::String(_), other) => {
+                Valid::succeed(other)
+            }
             (Value::Sequence(mut lhs), other) => match other {
                 Value::Sequence(rhs) => {
                     lhs.extend(rhs);
-                    Value::Sequence(lhs)
+                    Valid::succeed(Value::Sequence(lhs))
                 }
                 other => {
                     lhs.push(other);
-                    Value::Sequence(lhs)
+                    Valid::succeed(Value::Sequence(lhs))
                 }
             },
             (Value::Mapping(mut lhs), other) => match other {
                 Value::Mapping(rhs) => {
-                    for (key, mut value) in rhs {
+                    let mut errors = ValidationError::empty();
+                    for (key, other_value) in rhs {
                         if let Some(lhs_value) = lhs.remove(&key) {
-                            value = lhs_value.merge_right(value);
+                            match lhs_value.merge_right(other_value).to_result() {
+                                Ok(merged_value) => {
+                                    lhs.insert(key, merged_value);
+                                }
+                                Err(err) => {
+                                    errors = errors.combine(err);
+                                }
+                            }
+                        } else {
+                            lhs.insert(key, other_value);
                         }
-                        lhs.insert(key, value);
                     }
-                    Value::Mapping(lhs)
+
+                    if errors.is_empty() {
+                        Valid::succeed(Value::Mapping(lhs))
+                    } else {
+                        Valid::from_validation_err(errors)
+                    }
                 }
                 Value::Sequence(mut rhs) => {
                     rhs.push(Value::Mapping(lhs));
-                    Value::Sequence(rhs)
+                    Valid::succeed(Value::Sequence(rhs))
                 }
-                other => other,
+                other => Valid::succeed(other),
             },
             (Value::Tagged(mut lhs), other) => match other {
                 Value::Tagged(rhs) => {
                     if lhs.tag == rhs.tag {
-                        lhs.value = lhs.value.merge_right(rhs.value);
-                        Value::Tagged(lhs)
+                        lhs.value = lhs.value.merge_right(rhs.value)?;
+                        Valid::succeed(Value::Tagged(lhs))
                     } else {
-                        Value::Tagged(rhs)
+                        Valid::succeed(Value::Tagged(rhs))
                     }
                 }
-                other => other,
+                other => Valid::succeed(other),
             },
         }
     }
